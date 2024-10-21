4b1e71f3d4098da8962fcd67e775502d40cd68b2
JuninNha <166769562+JuninNha@users.noreply.github.com>
2024-10-21 10:25:33 -0300

    A

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ cartao.className = 'cartao';
 cartao.innerHTML =  ` 
 <div class="conteudo-cartao">
     <h3>${categoria}</h3>
-    <div class="pergunta-cartao"></div>
+    <div class="pergunta-cartao">
     <p>${pergunta}</p>
-    <div class="resposta-cartao"></div>
+    </div>
+    <div class="resposta-cartao">
     <p>${resposta}</p>
+    </div>
 </div>
 `
 container.appendChild(cartao);
